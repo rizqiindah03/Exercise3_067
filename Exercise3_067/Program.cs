23759c0793716bd6a038dc926032929f5f4f58c9
rizqiindah03 <rizqiindah32@gmail.com>
2022-11-25 16:05:20 +0700

    Add project files.

---
+++ Exercise3_067/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3_067
+{
+    class node 
+    {
+        /*create Nodes for the circular nextad list*/
+        public int rollNumber;
+        public string name;
+        public node next;
+    }
+
+}
