eb6605acf276ef8c11f321b7df0c85f0f0f91a92
rizqiindah03 <rizqiindah32@gmail.com>
2022-11-25 16:13:26 +0700

    membuat method search

--- Exercise3_067/Program.cs
+++ Exercise3_067/Program.cs
@@ -16,8 +16,24 @@ namespace Exercise3_067
     class Circularlist
     {
         node LAST;
+        public Circularlist()
+        {
+            LAST = null;
+        }
+        public bool Search(int rollNo, ref node previous, ref node current)/*searches for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);/*returns true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)/*if the node is present at the end*/
+                return true;
+            else
+                return (false);/*returns false is the node is not found*/
+        }
 
-       
+
     }
 
 }
